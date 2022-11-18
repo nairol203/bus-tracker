7c41d0f9a316e2b774f84d1dd02eeff94050e721
nairol203 <florian@nairol.me>
2022-11-18 18:06:20 +0100

    NO FONT-AWESOME

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,3 @@
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import Link from 'next/link';
 import styled from 'styled-components';
