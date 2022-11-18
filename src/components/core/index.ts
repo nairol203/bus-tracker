3c2120b3fcc013f6cf9cf2c73f37f119ae820684
nairol203 <florian@nairol.me>
2022-11-18 18:08:16 +0100

    BYE MODAL

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -1,6 +1,4 @@
 export { default as Checkbox } from './Checkbox';
-export { default as Dropdown } from './Dropdown';
-export { default as Modal } from './Modal';
 export { default as RadioButton } from './RadioButton';
 export { default as UrlInput } from './UrlInput';
 export { default as NumberInput } from './NumberInput';
