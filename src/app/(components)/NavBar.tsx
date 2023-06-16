b751c4d67c6a6c06da4401e2060df0740f1b5735
nairol203 <florian@nairol.me>
2023-06-16 14:52:14 +0200

    .

--- src/app/(components)/NavBar.tsx
+++ src/app/(components)/NavBar.tsx
@@ -52,9 +52,9 @@ export default function NavBar() {
 					<span className='sr-only'>Open main menu</span>
 					<svg className='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
 						<path
-							fill-rule='evenodd'
+							fillRule='evenodd'
 							d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
-							clip-rule='evenodd'
+							clipRule='evenodd'
 						></path>
 					</svg>
 				</button>
