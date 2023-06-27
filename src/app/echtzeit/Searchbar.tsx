cc0310ea40f61de3b38c42bdae9ca988f84e6e04
Florian Bock <f.bock.2003@gmail.com>
2023-06-27 19:52:12 +0200

    onchange => oninput

--- src/app/echtzeit/Searchbar.tsx
+++ src/app/echtzeit/Searchbar.tsx
@@ -29,7 +29,7 @@ export default function Searchbar({ selectedStop, setSelectedStop, setRouteId, s
 				<div className='relative w-full'>
 					<Combobox.Input
 						className='w-full rounded bg-white/80 p-2 dark:bg-white/10'
-						onChange={(event) => setQuery(event.currentTarget.value)}
+						onInput={(event) => setQuery(event.currentTarget.value)}
 						displayValue={(stop?: StopByCharacter) => stop?.name || ''}
 						placeholder='Suche nach einer Haltestelle'
 						autoFocus={!selectedStop}
