10ce410d523c4c1793138b9b2eac77ec613d8b96
Florian Bock <f.bock.2003@gmail.com>
2023-06-27 19:19:26 +0200

    a

--- src/app/echtzeit/Searchbar.tsx
+++ src/app/echtzeit/Searchbar.tsx
@@ -38,7 +38,7 @@ export default function Searchbar({ selectedStop, setSelectedStop, setRouteId, s
 						<Image src='/chevron-down.svg' alt='Arrow Down Icon' height={20} width={20} aria-hidden='true' className='dark:invert' />
 					</Combobox.Button>
 				</div>
-				<Transition
+				{/* <Transition
 					as={Fragment}
 					enter='transition ease-in duration-100'
 					enterFrom='opacity-0'
@@ -47,23 +47,23 @@ export default function Searchbar({ selectedStop, setSelectedStop, setRouteId, s
 					leaveFrom='opacity-100'
 					leaveTo='opacity-0'
 					afterLeave={() => setQuery('')}
-				>
-					<Combobox.Options className='absolute z-50 mt-1 w-full overflow-auto rounded bg-background shadow dark:bg-darkMode-background'>
-						{filteredStops.length === 0 && query !== '' ? (
-							<li className='wrap rounded bg-white/80 p-2 dark:bg-white/10'>Keine Ergebnisse</li>
-						) : (
-							filteredStops.map((stop) => (
-								<Combobox.Option
-									key={stop.id}
-									className={({ active }) => `${active ? 'bg-blue-600 text-white' : 'bg-white/80 dark:bg-white/10'} cursor-pointer p-2`}
-									value={stop}
-								>
-									{stop.name}
-								</Combobox.Option>
-							))
-						)}
-					</Combobox.Options>
-				</Transition>
+				> */}
+				<Combobox.Options className='z-50 mt-1 w-full overflow-auto rounded bg-background shadow dark:bg-darkMode-background'>
+					{filteredStops.length === 0 && query !== '' ? (
+						<li className='wrap rounded bg-white/80 p-2 dark:bg-white/10'>Keine Ergebnisse</li>
+					) : (
+						filteredStops.map((stop) => (
+							<Combobox.Option
+								key={stop.id}
+								className={({ active }) => `${active ? 'bg-blue-600 text-white' : 'bg-white/80 dark:bg-white/10'} cursor-pointer p-2`}
+								value={stop}
+							>
+								{stop.name}
+							</Combobox.Option>
+						))
+					)}
+				</Combobox.Options>
+				{/* </Transition> */}
 			</div>
 		</Combobox>
 	);
