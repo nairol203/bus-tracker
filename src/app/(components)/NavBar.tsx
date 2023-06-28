6f40896cc1fd4794bfab21b083e34c4636c54da0
Florian Bock <f.bock.2003@gmail.com>
2023-06-28 18:32:02 +0200

    headlessui navbar

--- src/app/(components)/NavBar.tsx
+++ src/app/(components)/NavBar.tsx
@@ -1,51 +1,21 @@
 'use client';
 
+import { Popover } from '@headlessui/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
 
 export default function NavBar() {
-	const [showDropdown, setDropdown] = useState(false);
-
-	function NavList() {
-		const pathname = usePathname();
-
-		return (
-			<div className='flex flex-col font-medium md:flex-row'>
-				<Link
-					onClick={() => setDropdown(false)}
-					href='/echtzeit'
-					className={`${
-						pathname === '/echtzeit' && 'rounded bg-blue-600 text-white underline-offset-4 md:bg-transparent md:text-black md:underline md:dark:text-white'
-					} m-1 px-4 py-2 md:m-0 md:hover:underline md:hover:underline-offset-4`}
-				>
-					Echtzeitabfahrten
-				</Link>
-				<Link
-					onClick={() => setDropdown(false)}
-					href='/favoriten'
-					className={`${
-						pathname === '/favoriten' && 'rounded bg-blue-600 text-white underline-offset-4 md:bg-transparent md:text-black md:underline md:dark:text-white'
-					} m-1 px-4 py-2 md:m-0 md:hover:underline md:hover:underline-offset-4`}
-				>
-					Favoriten
-				</Link>
-			</div>
-		);
-	}
+	const pathname = usePathname();
 
 	return (
-		<nav className='relative'>
-			<div className='mx-auto flex items-center justify-between px-2 py-4'>
-				<Link href='/' className='flex items-center '>
-					<Image src='/bus.svg' className='mr-3 rounded-full dark:invert' alt='KVG Bus Tracker Logo' width={30} height={30} />
-					<span className='self-center whitespace-nowrap text-2xl font-semibold dark:text-white'>KVG Bus Tracker</span>
-				</Link>
-				<button
-					onClick={() => setDropdown(!showDropdown)}
-					className='ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden'
-				>
+		<nav className='mx-auto flex items-center justify-between px-2 py-4'>
+			<Link href='/' className='flex items-center '>
+				<Image src='/bus.svg' className='mr-3 rounded-full dark:invert' alt='KVG Bus Tracker Logo' width={30} height={30} />
+				<span className='self-center whitespace-nowrap text-2xl font-semibold dark:text-white'>KVG Bus Tracker</span>
+			</Link>
+			<Popover>
+				<Popover.Button className='ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden'>
 					<span className='sr-only'>Open main menu</span>
 					<svg className='h-6 w-6' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
 						<path
@@ -54,16 +24,46 @@ export default function NavBar() {
 							clipRule='evenodd'
 						></path>
 					</svg>
-				</button>
-				<div className='hidden md:block'>
-					<NavList />
-				</div>
+				</Popover.Button>
+				<Popover.Panel className='absolute inset-[auto_0.5rem] z-50 mt-2 grid rounded bg-white p-1 font-medium shadow dark:bg-gray-500 md:hidden'>
+					<Popover.Button
+						as={Link}
+						href='/echtzeit'
+						className={`${
+							pathname === '/echtzeit' && 'rounded bg-blue-600 text-white underline-offset-4 md:bg-transparent md:text-black md:underline md:dark:text-white'
+						} m-1 px-4 py-2 md:m-0 md:hover:underline md:hover:underline-offset-4`}
+					>
+						Echtzeitabfahrten
+					</Popover.Button>
+					<Popover.Button
+						as={Link}
+						href='/favoriten'
+						className={`${
+							pathname === '/favoriten' && 'rounded bg-blue-600 text-white underline-offset-4 md:bg-transparent md:text-black md:underline md:dark:text-white'
+						} m-1 px-4 py-2 md:m-0 md:hover:underline md:hover:underline-offset-4`}
+					>
+						Favoriten
+					</Popover.Button>
+				</Popover.Panel>
+			</Popover>
+			<div className='hidden font-medium md:flex'>
+				<Link
+					href='/echtzeit'
+					className={`${
+						pathname === '/echtzeit' && 'rounded bg-blue-600 text-white underline-offset-4 md:bg-transparent md:text-black md:underline md:dark:text-white'
+					} m-1 px-4 py-2 md:m-0 md:hover:underline md:hover:underline-offset-4`}
+				>
+					Echtzeitabfahrten
+				</Link>
+				<Link
+					href='/favoriten'
+					className={`${
+						pathname === '/favoriten' && 'rounded bg-blue-600 text-white underline-offset-4 md:bg-transparent md:text-black md:underline md:dark:text-white'
+					} m-1 px-4 py-2 md:m-0 md:hover:underline md:hover:underline-offset-4`}
+				>
+					Favoriten
+				</Link>
 			</div>
-			{showDropdown && (
-				<div className='absolute inset-[auto_0.5rem] z-50 rounded bg-white p-1 shadow dark:bg-gray-500 md:hidden'>
-					<NavList />
-				</div>
-			)}
 		</nav>
 	);
 }
