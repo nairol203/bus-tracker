d24bb24780cb2d7feed76389eb0304bc5c5ba609
nairol203 <florian@nairol.me>
2023-10-13 13:52:11 +0200

    first attempt for v2

---
+++ src/app/v2/page.tsx
@@ -0,0 +1,34 @@
+import Departures from './Departures';
+
+const ONE_DAY_IN_SECONDS = 86_400;
+
+const alphabet = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));
+
+async function searchByCharacter(character: string): Promise<StopsByCharacter> {
+	const endpoint = new URL('https://www.kvg-kiel.de/internetservice/services/lookup/stopsByCharacter');
+
+	endpoint.searchParams.append('character', character);
+
+	const res = await fetch(endpoint.toString(), {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		next: {
+			revalidate: ONE_DAY_IN_SECONDS,
+		},
+	});
+
+	return res.json();
+}
+
+export default async function Searchbar() {
+	const stops: StopByCharacter[] = [];
+
+	for (const letter of alphabet) {
+		const result = await searchByCharacter(letter);
+		stops.push(...result.stops);
+	}
+
+	return <Departures stops={stops} />;
+}
