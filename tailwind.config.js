5bb37eb47fd516f1ec28ff26f6d75287508e2d7c
nairol203 <florian@nairol.me>
2023-10-13 16:41:16 +0200

    implementing the new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,20 @@
 module.exports = {
 	content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
-		extend: {
-			colors: {
-				background: 'rgb(226 236 255)',
-				darkMode: {
-					background: 'rgb(19, 26, 40)',
-				},
+		colors: {
+			text: '#010813',
+			background: '#e2ecff',
+			primary: '#0A3380',
+			secondary: '#AEC8F9',
+			accent: '#0F51CC',
+			skeleton: '#d1d5db',
+			darkMode: {
+				text: '#ecf3fe',
+				background: '#000b1f',
+				primary: '#7fa8f5',
+				secondary: '#062051	',
+				accent: '#3375f0',
+				skeleton: '#374151',
 			},
 		},
 	},
