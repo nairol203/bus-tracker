79da943c856a8fa4550e145b783c9bd4fb4e975e
Florian Bock <f.bock.2003@gmail.com>
2023-10-15 19:39:41 +0200

    changed color

--- src/app/(components)/HealthIndicator.tsx
+++ src/app/(components)/HealthIndicator.tsx
@@ -4,8 +4,8 @@ export default function HealthIndicator({ isError, isFetching, isPaused }: { isF
 			<div className='flex items-center gap-2 rounded-full bg-secondary px-2.5 py-1 shadow dark:bg-darkMode-secondary'>
 				<span>Offline</span>
 				<span className='relative flex h-3 w-3'>
-					<span className={`${isFetching && 'animate-ping'} absolute inline-flex h-full w-full rounded-full bg-accent opacity-75 dark:bg-darkMode-accent`}></span>
-					<span className={'relative inline-flex h-3 w-3 rounded-full bg-primary dark:bg-darkMode-primary'}></span>
+					<span className={`${isFetching && 'animate-ping'} absolute inline-flex h-full w-full rounded-full bg-primary opacity-75 dark:bg-darkMode-primary`}></span>
+					<span className={'relative inline-flex h-3 w-3 rounded-full bg-accent dark:bg-darkMode-accent'}></span>
 				</span>
 			</div>
 		);
@@ -15,8 +15,8 @@ export default function HealthIndicator({ isError, isFetching, isPaused }: { isF
 			<div className='flex items-center gap-2 rounded-full bg-secondary px-2.5 py-1 shadow dark:bg-darkMode-secondary'>
 				<span>Error</span>
 				<span className='relative flex h-3 w-3'>
-					<span className={`${isFetching && 'animate-ping'} absolute inline-flex h-full w-full rounded-full bg-accent opacity-75 dark:bg-darkMode-accent`}></span>
-					<span className={'relative inline-flex h-3 w-3 rounded-full bg-primary dark:bg-darkMode-primary'}></span>
+					<span className={`${isFetching && 'animate-ping'} absolute inline-flex h-full w-full rounded-full bg-primary opacity-75 dark:bg-darkMode-primary`}></span>
+					<span className={'relative inline-flex h-3 w-3 rounded-full bg-accent dark:bg-darkMode-accent'}></span>
 				</span>
 			</div>
 		);
@@ -24,8 +24,8 @@ export default function HealthIndicator({ isError, isFetching, isPaused }: { isF
 	return (
 		<div className='px-2.5 py-1'>
 			<span className='relative flex h-3 w-3'>
-				<span className={`${isFetching && 'animate-ping'} absolute inline-flex h-full w-full rounded-full bg-accent opacity-75 dark:bg-darkMode-accent`}></span>
-				<span className={`relative inline-flex h-3 w-3 rounded-full bg-primary dark:bg-darkMode-primary`}></span>
+				<span className={`${isFetching && 'animate-ping'} absolute inline-flex h-full w-full rounded-full bg-primary opacity-75 dark:bg-darkMode-primary`}></span>
+				<span className={`relative inline-flex h-3 w-3 rounded-full bg-accent dark:bg-darkMode-accent`}></span>
 			</span>
 		</div>
 	);
