bf247332cdf30fb470827389695894dbdd165784
Florian Bock <f.bock.2003@gmail.com>
2023-10-15 18:56:53 +0200

    added suggestion button when selecting route

--- src/app/echtzeit/RouteFilter.tsx
+++ src/app/echtzeit/RouteFilter.tsx
@@ -3,6 +3,12 @@ import Image from 'next/image';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { Fragment, useCallback, useState } from 'react';
 
+function filterUniqueAndSortAscending(arr: string[]) {
+	const uniqueArr = Array.from(new Set(arr));
+	const sortedArr = uniqueArr.sort();
+	return sortedArr;
+}
+
 export default function RouteFilter({ stop }: { stop: KVGStops }) {
 	const router = useRouter();
 	const pathname = usePathname();
@@ -12,8 +18,13 @@ export default function RouteFilter({ stop }: { stop: KVGStops }) {
 	const direction = searchParams.get('direction');
 
 	const routes = direction ? stop.routes.filter((route) => route.directions.includes(direction)) : stop.routes;
-
 	const [selectedRoute, setSelectedRoute] = useState<Route | null>(routes.find((route) => route.id === routeId) ?? null);
+
+	let directions = routeId ? stop.routes.find((route) => route.id === routeId)!.directions : [];
+	if (!routeId) {
+		directions.shift();
+	}
+	directions = filterUniqueAndSortAscending(directions);
 
 	const createQueryString = useCallback(
 		(name: string, value: string) => {
@@ -36,61 +47,80 @@ export default function RouteFilter({ stop }: { stop: KVGStops }) {
 	);
 
 	return (
-		<Listbox
-			value={selectedRoute}
-			onChange={(value) => {
-				if (selectedRoute === value) {
-					setSelectedRoute(null);
-				} else {
-					setSelectedRoute(value);
-				}
-			}}
-			as={Fragment}
-		>
-			<div className='relative'>
-				<Listbox.Button
-					className={`${
-						selectedRoute
-							? 'bg-primary dark:bg-darkMode-primary dark:text-text'
-							: 'bg-secondary dark:bg-darkMode-secondary md:hover:bg-accent md:hover:text-darkMode-text dark:md:hover:bg-darkMode-accent'
-					} flex items-center gap-2 px-2 py-1 rounded shadow transition duration-200`}
-				>
-					{selectedRoute ? `${selectedRoute.authority} ${selectedRoute.name}` : 'Linie'}
-					<Image src='/chevron-down.svg' height={15} width={15} alt='Chevron down icon' className={selectedRoute ? '' : 'dark:invert'} />
-				</Listbox.Button>
-				<Transition
-					as={Fragment}
-					enter='transition ease-in duration-100'
-					enterFrom='opacity-0'
-					enterTo='opacity-100'
-					leave='transition ease-in duration-100'
-					leaveFrom='opacity-100'
-					leaveTo='opacity-0'
-				>
-					<Listbox.Options className='absolute mt-1 grid gap-2 bg-secondary dark:bg-darkMode-secondary shadow rounded max-h-96 overflow-y-auto w-60'>
-						{routes.map((route) => (
-							<Listbox.Option key={route.id} value={route} as={Fragment}>
-								{({ active, selected }) => (
-									<button
-										onClick={() => {
-											if (routeId == route.id) {
-												router.push(pathname + '?' + removeQueryStrings(['routeId']));
-											} else {
-												router.push(pathname + '?' + createQueryString('routeId', route.id));
-											}
-										}}
-										className={`${active && 'bg-accent text-darkMode-text dark:bg-darkMode-accent'} ${
-											selected && 'bg-accent text-darkMode-text dark:bg-darkMode-accent'
-										} px-2 py-1 rounded text-start`}
-									>
-										{route.authority} {route.name}
-									</button>
-								)}
-							</Listbox.Option>
-						))}
-					</Listbox.Options>
-				</Transition>
-			</div>
-		</Listbox>
+		<>
+			<Listbox
+				value={selectedRoute}
+				onChange={(value) => {
+					if (selectedRoute === value) {
+						setSelectedRoute(null);
+					} else {
+						setSelectedRoute(value);
+					}
+				}}
+				as={Fragment}
+			>
+				<div className='relative'>
+					<Listbox.Button
+						className={`${
+							selectedRoute
+								? 'bg-primary dark:bg-darkMode-primary dark:text-text'
+								: 'bg-secondary dark:bg-darkMode-secondary md:hover:bg-accent md:hover:text-darkMode-text dark:md:hover:bg-darkMode-accent'
+						} flex items-center gap-2 px-2 py-1 rounded shadow transition duration-200`}
+					>
+						{selectedRoute ? `${selectedRoute.authority} ${selectedRoute.name}` : 'Linie'}
+						<Image src='/chevron-down.svg' height={15} width={15} alt='Chevron down icon' className={selectedRoute ? '' : 'dark:invert'} />
+					</Listbox.Button>
+					<Transition
+						as={Fragment}
+						enter='transition ease-in duration-100'
+						enterFrom='opacity-0'
+						enterTo='opacity-100'
+						leave='transition ease-in duration-100'
+						leaveFrom='opacity-100'
+						leaveTo='opacity-0'
+					>
+						<Listbox.Options className='absolute mt-1 grid gap-2 bg-secondary dark:bg-darkMode-secondary shadow rounded max-h-96 overflow-y-auto w-60'>
+							{routes.map((route) => (
+								<Listbox.Option key={route.id} value={route} as={Fragment}>
+									{({ active, selected }) => (
+										<button
+											onClick={() => {
+												if (routeId == route.id) {
+													router.push(pathname + '?' + removeQueryStrings(['routeId', 'direction']));
+												} else {
+													router.push(pathname + '?' + createQueryString('routeId', route.id));
+												}
+											}}
+											className={`${active && 'bg-accent text-darkMode-text dark:bg-darkMode-accent'} ${
+												selected && 'bg-accent text-darkMode-text dark:bg-darkMode-accent'
+											} px-2 py-1 rounded text-start`}
+										>
+											{route.authority} {route.name}
+										</button>
+									)}
+								</Listbox.Option>
+							))}
+						</Listbox.Options>
+					</Transition>
+				</div>
+			</Listbox>
+			{selectedRoute &&
+				directions.map((_direction) => (
+					<button
+						onClick={() => {
+							if (direction === _direction) {
+								router.push(pathname + '?' + removeQueryStrings(['direction']));
+							} else {
+								router.push(pathname + '?' + createQueryString('direction', _direction));
+							}
+						}}
+						className={`${
+							direction === _direction ? 'bg-primary dark:text-text dark:bg-darkMode-primary' : 'bg-secondary dark:bg-darkMode-secondary'
+						} px-2 py-1 rounded text-start`}
+					>
+						{_direction}
+					</button>
+				))}
+		</>
 	);
 }
