49e8692a20491bad9ff5ddcdfb8730dae9b019b9
nairol203 <florian@nairol.me>
2023-10-18 14:38:14 +0200

    test2

--- src/app/echtzeit/RouteFilter.tsx
+++ src/app/echtzeit/RouteFilter.tsx
@@ -78,62 +78,65 @@ export default function RouteFilter({ stop }: { stop: KVGStops }) {
 				as={Fragment}
 				disabled={routes.length <= 1}
 			>
-				<div className='md:relative'>
-					<Listbox.Button
-						className={`${
-							selectedRoute && selectedRoute.id !== 'all'
-								? 'bg-primary text-darkMode-text dark:bg-darkMode-primary dark:text-text'
-								: 'group bg-secondary dark:bg-darkMode-secondary md:enabled:hover:bg-accent md:enabled:hover:text-darkMode-text dark:md:enabled:hover:bg-darkMode-accent'
-						} flex items-center gap-2 rounded px-2 py-1 shadow transition duration-200`}
-					>
-						{`${selectedRoute.authority} ${selectedRoute.name}`}
-						<Image
-							src='/chevron-up-down.svg'
-							height={15}
-							width={15}
-							alt='Chevron down icon'
-							className={selectedRoute && selectedRoute.id !== 'all' ? 'invert dark:invert-0' : 'md:group-enabled:group-hover:invert dark:invert'}
-						/>
-					</Listbox.Button>
-					<Transition
-						as={Fragment}
-						enter='transition ease-in duration-100'
-						enterFrom='opacity-0'
-						enterTo='opacity-100'
-						leave='transition ease-in duration-100'
-						leaveFrom='opacity-100'
-						leaveTo='opacity-0'
-					>
-						<Listbox.Options className='absolute bottom-0 left-0 right-0 top-[12.5arem] md:mt-1 md:max-h-96 md:w-60 overflow-y-auto rounded bg-secondary shadow dark:bg-darkMode-secondary'>
-							{routes.map((route) => (
-								<Listbox.Option key={route.id} value={route} as={Fragment}>
-									{({ active }) => (
-										<li
-											className={`${
-												active && 'bg-accent text-darkMode-text dark:bg-darkMode-accent'
-											} group flex gap-3 p-3 md:px-2 md:py-1.5 text-start cursor-default`}
-										>
-											{routeId === route.id || (!routeId && route.id === 'all') ? (
-												<Image
-													src='/check.svg'
-													height={15}
-													width={15}
-													alt='Check Icon'
-													className={`${active ? 'invert' : ''} group-hover:invert dark:invert`}
-												/>
-											) : (
-												<span className='h-[15px] w-[15px]' />
-											)}
-											<span>
-												{route.authority} {route.name}
-											</span>
-										</li>
-									)}
-								</Listbox.Option>
-							))}
-						</Listbox.Options>
-					</Transition>
-				</div>
+				{({ open }) => (
+					<div className='md:relative'>
+						<Listbox.Button
+							className={`${
+								selectedRoute && selectedRoute.id !== 'all'
+									? 'bg-primary text-darkMode-text dark:bg-darkMode-primary dark:text-text'
+									: 'group bg-secondary dark:bg-darkMode-secondary md:enabled:hover:bg-accent md:enabled:hover:text-darkMode-text dark:md:enabled:hover:bg-darkMode-accent'
+							} flex items-center gap-2 rounded px-2 py-1 shadow transition duration-200`}
+						>
+							{`${selectedRoute.authority} ${selectedRoute.name}`}
+							<Image
+								src='/chevron-up-down.svg'
+								height={15}
+								width={15}
+								alt='Chevron down icon'
+								className={selectedRoute && selectedRoute.id !== 'all' ? 'invert dark:invert-0' : 'md:group-enabled:group-hover:invert dark:invert'}
+							/>
+						</Listbox.Button>
+						{open && <span className='z-10 absolute inset-0 backdrop-blur-sm' />}
+						<Transition
+							as={Fragment}
+							enter='transition ease-in duration-100'
+							enterFrom='opacity-0'
+							enterTo='opacity-100'
+							leave='transition ease-in duration-100'
+							leaveFrom='opacity-100'
+							leaveTo='opacity-0'
+						>
+							<Listbox.Options className='z-20 absolute bottom-0 left-0 right-0 top-1/3 md:mt-1 md:max-h-96 md:w-60 overflow-y-auto rounded bg-secondary shadow-2xl dark:bg-darkMode-secondary'>
+								{routes.map((route) => (
+									<Listbox.Option key={route.id} value={route} as={Fragment}>
+										{({ active }) => (
+											<li
+												className={`${
+													active && 'bg-accent text-darkMode-text dark:bg-darkMode-accent'
+												} group flex gap-3 p-3 md:px-2 md:py-1.5 text-start cursor-default`}
+											>
+												{routeId === route.id || (!routeId && route.id === 'all') ? (
+													<Image
+														src='/check.svg'
+														height={15}
+														width={15}
+														alt='Check Icon'
+														className={`${active ? 'invert' : ''} group-hover:invert dark:invert`}
+													/>
+												) : (
+													<span className='h-[15px] w-[15px]' />
+												)}
+												<span>
+													{route.authority} {route.name}
+												</span>
+											</li>
+										)}
+									</Listbox.Option>
+								))}
+							</Listbox.Options>
+						</Transition>
+					</div>
+				)}
 			</Listbox>
 			{selectedRoute &&
 				directions.map((_direction, index) => (
