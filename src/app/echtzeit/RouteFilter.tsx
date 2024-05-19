467e45b59a313fc88c0888078751431feb43d405
nairol203 <florian@nairol.me>
2024-05-19 11:53:24 +0200

    nextjs + headlessui update

--- src/app/echtzeit/RouteFilter.tsx
+++ src/app/echtzeit/RouteFilter.tsx
@@ -1,4 +1,4 @@
-import { Listbox, Transition } from '@headlessui/react';
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions, Transition } from '@headlessui/react';
 import Image from 'next/image';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { Fragment, useCallback } from 'react';
@@ -60,7 +60,7 @@ export default function RouteFilter({ stop }: { stop: KVGStops }) {
 		>
 			{({ open }) => (
 				<div className='md:relative'>
-					<Listbox.Button
+					<ListboxButton
 						className={`${
 							selectedRoute && selectedRoute.id !== 'all'
 								? 'bg-primary text-darkMode-text dark:bg-darkMode-primary dark:text-text'
@@ -75,7 +75,8 @@ export default function RouteFilter({ stop }: { stop: KVGStops }) {
 							alt='Chevron down icon'
 							className={selectedRoute && selectedRoute.id !== 'all' ? 'invert dark:invert-0' : 'md:group-enabled:group-hover:invert dark:invert'}
 						/>
-					</Listbox.Button>
+					</ListboxButton>
+					{open && <span className='z-10 absolute inset-0 bg-text/30 dark:bg-text/60 md:hidden' />}
 					<Transition
 						enter='transition ease-in duration-100'
 						enterFrom='opacity-0'
@@ -84,15 +85,14 @@ export default function RouteFilter({ stop }: { stop: KVGStops }) {
 						leaveFrom='opacity-100'
 						leaveTo='opacity-0'
 					>
-						{open && <span className='z-10 absolute inset-0 bg-text/30 dark:bg-text/60 md:hidden' />}
-						<Listbox.Options className='z-20 absolute top-1/3 inset-x-0 bottom-0 md:inset-auto md:mt-1 md:max-h-96 md:w-60 overflow-y-auto bg-background md:bg-secondary shadow-lg dark:bg-darkMode-background dark:md:bg-darkMode-secondary overscroll-contain pt-3 md:pt-0 rounded-t-2xl md:rounded-t-sm md:rounded-sm'>
+						<ListboxOptions className='transition z-20 absolute top-1/3 inset-x-0 bottom-0 md:inset-auto md:mt-1 md:max-h-96 md:w-60 overflow-y-auto bg-background md:bg-secondary shadow-lg dark:bg-darkMode-background dark:md:bg-darkMode-secondary overscroll-contain pt-3 md:pt-0 rounded-t-2xl md:rounded-t-sm md:rounded-sm'>
 							<h2 className='mx-4 my-2 md:hidden'>Linienfilter</h2>
 							{routes.map((route) => (
-								<Listbox.Option key={route.id} value={route} as={Fragment}>
-									{({ active }) => (
+								<ListboxOption key={route.id} value={route} as={Fragment}>
+									{({ focus }) => (
 										<li
 											className={`${
-												active && 'md:bg-accent  md:text-darkMode-text md:dark:bg-darkMode-accent'
+												focus && 'md:bg-accent  md:text-darkMode-text md:dark:bg-darkMode-accent'
 											} group flex gap-3 p-3 md:px-2 md:py-1.5 text-start cursor-default`}
 										>
 											{routeId === route.id || (!routeId && route.id === 'all') ? (
@@ -101,7 +101,7 @@ export default function RouteFilter({ stop }: { stop: KVGStops }) {
 													height={15}
 													width={15}
 													alt='Check Icon'
-													className={`${active ? 'md:invert' : ''} md:group-hover:invert dark:invert`}
+													className={`${focus ? 'md:invert' : ''} md:group-hover:invert dark:invert`}
 												/>
 											) : (
 												<Image
@@ -109,7 +109,7 @@ export default function RouteFilter({ stop }: { stop: KVGStops }) {
 													height={15}
 													width={15}
 													alt='Check Icon'
-													className={`${active ? 'md:invert' : ''} md:group-hover:invert dark:invert`}
+													className={`${focus ? 'md:invert' : ''} md:group-hover:invert dark:invert`}
 												/>
 											)}
 											<span>
@@ -117,9 +117,9 @@ export default function RouteFilter({ stop }: { stop: KVGStops }) {
 											</span>
 										</li>
 									)}
-								</Listbox.Option>
+								</ListboxOption>
 							))}
-						</Listbox.Options>
+						</ListboxOptions>
 					</Transition>
 				</div>
 			)}
