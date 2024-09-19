e02b1d50fa1d2eb14a3722135e9be82a09454ee2
nairol203 <florian@nairol.me>
2024-09-19 10:11:31 +0200

    moved from dropdown to dialog

--- src/app/(components)/SettingsMenu.tsx
+++ src/app/(components)/SettingsMenu.tsx
@@ -1,28 +1,42 @@
 'use client';
 
 import useLocalStorage from '@/utils/useSessionStorage';
-import { Menu, MenuButton, MenuItem, MenuItems, Switch } from '@headlessui/react';
+import { CloseButton, Dialog, DialogBackdrop, DialogPanel, DialogTitle, Switch } from '@headlessui/react';
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function SettingsMenu() {
+	const [isOpen, setIsOpen] = useState(false);
 	const [useRelativeTimes, setRelativeTimes] = useLocalStorage<boolean>('useRelativeTimes', false);
 
 	return (
-		<Menu>
-			<MenuButton className='rounded bg-secondary text-text px-2.5 py-1.5 dark:bg-darkMode-secondary dark:text-darkMode-text md:hover:bg-accent md:hover:text-darkMode-text dark:md:hover:bg-darkMode-accent'>
-				Einstellungen
-			</MenuButton>
-			<MenuItems anchor='bottom end' className='grid rounded bg-secondary p-2 mt-1 shadow'>
-				<MenuItem as='div' className='flex gap-2'>
-					Benutze Relative Zeiten
-					<Switch
-						checked={useRelativeTimes}
-						onChange={setRelativeTimes}
-						className='group inline-flex h-6 w-11 items-center rounded-full bg-primary transition data-[checked]:bg-accent'
-					>
-						<span className='size-4 translate-x-1 rounded-full bg-darkMode-text transition group-data-[checked]:translate-x-6' />
-					</Switch>
-				</MenuItem>
-			</MenuItems>
-		</Menu>
+		<>
+			<button onClick={() => setIsOpen(true)} className='rounded p-2 md:hover:bg-secondary md:hover:text-darkMode-text dark:md:hover:bg-darkMode-secondary'>
+				<Image src='/gear.svg' alt='Settings Icon' height={20} width={20} className='shrink-0 dark:invert' />
+			</button>
+			<Dialog open={isOpen} onClose={() => setIsOpen(false)} className='relative z-50'>
+				<DialogBackdrop className='fixed inset-0 bg-text/30' />
+				<div className='fixed inset-0 flex w-screen items-center justify-center p-4'>
+					<DialogPanel className='max-w-lg min-w-80 min-h-80 space-y-4 rounded bg-darkMode-text p-4'>
+						<div className='flex gap-2 justify-between'>
+							<DialogTitle className='font-bold'>Einstellungen</DialogTitle>
+							<CloseButton>
+								<Image src='/xmark.svg' alt='Close Icon' height={20} width={20} className='shrink-0 dark:invert' />
+							</CloseButton>
+						</div>
+						<div className='flex justify-between items-center gap-2'>
+							<span>Benutze relative Zeiten</span>
+							<Switch
+								checked={useRelativeTimes}
+								onChange={setRelativeTimes}
+								className='group inline-flex h-6 w-11 items-center rounded-full bg-primary transition data-[checked]:bg-accent'
+							>
+								<span className='size-4 translate-x-1 rounded-full bg-darkMode-text transition group-data-[checked]:translate-x-6' />
+							</Switch>
+						</div>
+					</DialogPanel>
+				</div>
+			</Dialog>
+		</>
 	);
 }
