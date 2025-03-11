73dd8b90916cca3ed3023ffa403857dd95eddba9
Florian Bock <florian@nairol.me>
2025-03-11 17:52:44 +0100

    readd margin & grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Searchbar from './(components)/Searchbar';
 
 export default function Page() {
 	return (
-		<div>
+		<div className='mx-2 grid gap-2'>
 			<Searchbar />
 			<RecommendedSearches />
 		</div>
