e1599cddbfa5d6cb2a3dfb955520d048ea127cc3
Florian Bock <florian@nairol.me>
2025-03-13 10:03:38 +0100

    revert filtering change

--- src/app/(components)/KVGTable.tsx
+++ src/app/(components)/KVGTable.tsx
@@ -68,26 +68,22 @@ export default function KVGTable({
 }) {
 	const { useRelativeTimes } = useBusStore();
 
-	const filteredActual = data.actual.filter((stop) => (direction ? stop.direction === direction : true && routeId ? stop.routeId === routeId : true));
-
 	return (
 		<div className='grid gap-1'>
 			{showGeneralAlerts && <GeneralAlerts data={data} />}
 			{showRouteAlerts && <RouteAlerts data={data} direction={direction} routeId={routeId} />}
-			{filteredActual.length ? (
-				filteredActual
-					.filter((stop) => (direction ? stop.direction === direction : true && routeId ? stop.routeId === routeId : true))
-					.map((actual, index) => (
-						<Link
-							href={`/trip/${actual.tripId}`}
-							className='grid grid-cols-[35px_1fr_75px] justify-between gap-2 rounded bg-secondary p-2 shadow transition duration-200 md:hover:bg-accent md:hover:text-darkMode-text dark:bg-darkMode-secondary dark:md:hover:bg-darkMode-accent'
-							key={`${index}-${actual.tripId}`}
-						>
-							<span className='rounded-lg bg-accent text-center text-darkMode-text dark:bg-darkMode-accent'>{actual.patternText}</span>
-							<span className='whitespace-nowrap'>{actual.direction}</span>
-							{formatDepartureTime(actual, isPaused, useRelativeTimes)}
-						</Link>
-					))
+			{data.actual.length ? (
+				data.actual.map((actual, index) => (
+					<Link
+						href={`/trip/${actual.tripId}`}
+						className='grid grid-cols-[35px_1fr_75px] justify-between gap-2 rounded bg-secondary p-2 shadow transition duration-200 md:hover:bg-accent md:hover:text-darkMode-text dark:bg-darkMode-secondary dark:md:hover:bg-darkMode-accent'
+						key={`${index}-${actual.tripId}`}
+					>
+						<span className='rounded-lg bg-accent text-center text-darkMode-text dark:bg-darkMode-accent'>{actual.patternText}</span>
+						<span className='whitespace-nowrap'>{actual.direction}</span>
+						{formatDepartureTime(actual, isPaused, useRelativeTimes)}
+					</Link>
+				))
 			) : (
 				<div className='rounded bg-secondary p-2 shadow dark:bg-darkMode-secondary'>Keine Daten</div>
 			)}
