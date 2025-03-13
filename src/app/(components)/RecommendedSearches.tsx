f3784869b1f292cbd11c8b761b506928f1b20003
nairol203 <florian@nairol.me>
2025-03-13 11:40:33 +0100

    Update to new design

--- src/app/(components)/RecommendedSearches.tsx
+++ src/app/(components)/RecommendedSearches.tsx
@@ -11,62 +11,51 @@ export default function RecommendedSearches() {
 	return (
 		<>
 			{lastSearches.length ? (
-				<div className='mt-2 grid gap-2'>
+				<div className='mt-2 grid gap-1'>
 					<h2>Letzte Suchanfragen</h2>
 					{lastSearches
 						.toReversed()
 						.slice(0, 10)
 						.map((stop, index) => (
-							<Link
-								className='group flex gap-3	 rounded bg-secondary p-2 shadow transition duration-200 md:hover:bg-accent md:hover:text-darkMode-text dark:bg-darkMode-secondary dark:md:hover:bg-darkMode-accent'
-								href={`/stop/${stop.number}`}
-								key={`${index}_${stop.id}`}
-							>
-								<Image src='/clock-rotate-left.svg' height={20} width={20} alt='Clock rotate left icon' className='md:group-hover:invert dark:invert' />
-								{stop.name}
-							</Link>
+							<>
+								<Link className='group flex gap-3	p-2' href={`/stop/${stop.number}`} key={`${index}_${stop.id}`}>
+									<Image src='/clock-rotate-left.svg' height={20} width={20} alt='Clock rotate left icon' className='md:group-hover:invert dark:invert' />
+									{stop.name}
+								</Link>
+								<div className='border-t border-primary' />
+							</>
 						))}
 				</div>
 			) : (
 				<></>
 			)}
-			<div className='mt-2 grid gap-2'>
+			<div className='mt-2 grid gap-1'>
 				<h2>Beliebte Suchanfragen</h2>
-				<Link
-					className='group flex gap-3	 rounded bg-secondary p-2 shadow transition duration-200 md:hover:bg-accent md:hover:text-darkMode-text dark:bg-darkMode-secondary dark:md:hover:bg-darkMode-accent'
-					href={'/stop/2387'}
-				>
+				<Link className='group flex gap-3	p-2' href={'/stop/2387'}>
 					<Image src='/arrow-up-trend.svg' height={20} width={20} alt='Clock rotate left icon' className='md:group-hover:invert dark:invert' />
 					{stops.find((stop) => stop.number === '2387')?.name || 'Unbekannte Haltestelle'}
 				</Link>
-				<Link
-					className='group flex gap-3	 rounded bg-secondary p-2 shadow transition duration-200 md:hover:bg-accent md:hover:text-darkMode-text dark:bg-darkMode-secondary dark:md:hover:bg-darkMode-accent'
-					href={'/stop/1624'}
-				>
+				<div className='border-t border-primary' />
+				<Link className='group flex gap-3	p-2' href={'/stop/1624'}>
 					<Image src='/arrow-up-trend.svg' height={20} width={20} alt='Clock rotate left icon' className='md:group-hover:invert dark:invert' />
 					{stops.find((stop) => stop.number === '1624')?.name || 'Unbekannte Haltestelle'}
 				</Link>
-				<Link
-					className='group flex gap-3	 rounded bg-secondary p-2 shadow transition duration-200 md:hover:bg-accent md:hover:text-darkMode-text dark:bg-darkMode-secondary dark:md:hover:bg-darkMode-accent'
-					href={'/stop/1312'}
-				>
+				<div className='border-t border-primary' />
+				<Link className='group flex gap-3	p-2' href={'/stop/1312'}>
 					<Image src='/arrow-up-trend.svg' height={20} width={20} alt='Clock rotate left icon' className='md:group-hover:invert dark:invert' />
 					{stops.find((stop) => stop.number === '1312')?.name || 'Unbekannte Haltestelle'}
 				</Link>
-				<Link
-					className='group flex gap-3	 rounded bg-secondary p-2 shadow transition duration-200 md:hover:bg-accent md:hover:text-darkMode-text dark:bg-darkMode-secondary dark:md:hover:bg-darkMode-accent'
-					href={'/stop/2246'}
-				>
+				<div className='border-t border-primary' />
+				<Link className='group flex gap-3	p-2' href={'/stop/2246'}>
 					<Image src='/arrow-up-trend.svg' height={20} width={20} alt='Clock rotate left icon' className='md:group-hover:invert dark:invert' />
 					{stops.find((stop) => stop.number === '2246')?.name || 'Unbekannte Haltestelle'}
 				</Link>
-				<Link
-					className='group flex gap-3	 rounded bg-secondary p-2 shadow transition duration-200 md:hover:bg-accent md:hover:text-darkMode-text dark:bg-darkMode-secondary dark:md:hover:bg-darkMode-accent'
-					href={'/stop/1317'}
-				>
+				<div className='border-t border-primary' />
+				<Link className='group flex gap-3	p-2' href={'/stop/1317'}>
 					<Image src='/arrow-up-trend.svg' height={20} width={20} alt='Clock rotate left icon' className='md:group-hover:invert dark:invert' />
 					{stops.find((stop) => stop.number === '1317')?.name || 'Unbekannte Haltestelle'}
 				</Link>
+				<div className='border-t border-primary' />
 			</div>
 		</>
 	);
