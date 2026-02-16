3552765a57e906a16bb0b94ab3239441af60012c
Florian Bock <florian@nairol.de>
2026-02-16 16:38:31 +0100

    chore: add base url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import Link from 'next/link';
 import SettingsMenu from './(components)/SettingsMenu';
 
 export const metadata: Metadata = {
+	metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'),
 	title: 'KVG Bus Tracker',
 	description: 'Aktuelle Abfahrtszeiten aller Buslinien der KVG Kiel. Echtzeit-Infos, alle Buslinien und Verspätungen auf einen Blick.',
 	authors: [
